168b4d8f7483448bd594660c9d7c97c204b767c4
Jay Gangwar <jaygangwar86@gmail.com>
2026-06-14 20:34:09 +0530

    DB Models closes #3

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -6,6 +6,13 @@ const UserSchema = new Schema(
     username: { type: String, required: true },
     clerkId: { type: String, unique: true, required: true },
     postsQueried: [{ type: String }],
+    plan: {
+      type: String,
+      required: true,
+      enum: ["free", "pro"],
+      default: "free",
+    }, //? Add more later if more plans are added(hopefully 🤞)
+    credits: { type: Number, default: 0 },
   },
   {
     timestamps: true,
