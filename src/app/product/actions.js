c0b5f032ae457560c3b87828e417456e4fe40bac
Jay Gangwar <jaygangwar86@gmail.com>
2026-06-15 20:43:42 +0530

    Subreddit suggestions done closes #5

--- src/app/product/actions.js
+++ src/app/product/actions.js
@@ -13,10 +13,27 @@ export async function submit(competitors, productDescription) {
   // Combine the product description with the valid competitors
   const inputs = [productDescription, ...validCompetitors];
 
-  const response = await client.embeddings.create({
+  const embed = await client.embeddings.create({
     model: "mistral-embed-2312",
     inputs: inputs,
   });
 
-  console.log(response.data[0].embedding);
+  const response = await client.chat.complete({
+    model: "mistral-small-latest",
+    messages: [
+      {
+        content: `Give 5 subreddits where the founder may find customers of ${productDescription}. Only output the subreddits. Only give "subreddits". Prefix of r/`,
+        role: "system",
+      },
+    ],
+    responseFormat: {
+      type: "json_object",
+    },
+  });
+
+  const data = JSON.parse(response.choices[0].message.content);
+
+  console.log(data);
+
+  //   console.log(embed.data[0].embedding);
 }
